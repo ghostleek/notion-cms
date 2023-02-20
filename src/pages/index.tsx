7b7b6916372c57c08475d35ca6fc93395d9cfa88
Kahhow Lee <kahhowlee@Kahhows-MBP.lan>
2023-02-21 00:28:27 +0800

    pull block child page titles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,32 @@
-const Home = () => {
-  return <p>{JSON.stringify(quote)}</p>
+import { Client } from "@notionhq/client";
+
+
+const RecipePage = ({ recipes }) => {
+  return recipes.map((recipe) => <p>{recipe}</p>);
 };
 
 export const getStaticProps = async () => {
-  // fetch data
-  const quote = await fetch(
-      "https://pokeapi.co/api/v2/pokemon/charizard"
-  ).then(resp => resp.json());
+  const notion = new Client({
+    auth: process.env.NOTION_SECRET,
+  });
+
+  const data = await notion.blocks.children.list({
+    block_id: process.env.PAGE_ID,
+  });
+
+  const recipes = [];
+
+  data.results.forEach((result) => {
+    if (result.type === "child_page") {
+      recipes.push(result.child_page.title);
+    }
+  });
 
   return {
-      props: {
-          quote,
-      },
+    props: {
+      recipes,
+    },
   };
 };
+
+export default RecipePage;
